a14dae66dd878234758ae5767d1170e01b2e81d0
Jenny Bedwell <104868749+jennytrinh96@users.noreply.github.com>
2022-06-28 15:02:29 -0400

    update main.py
    csv path and reader created

--- PyBank/main.py
+++ PyBank/main.py
@@ -5,6 +5,20 @@
 import csv
 import os
 
+# Set up file path for csv file
+csv_path = os.path.join("..", "Resources", "budget_data.csv")
+# print(csv_path) Check path, Path is good to go
+
+# FIRST: Open csv file so we can search and do analysis
+with open(csv_path, 'r') as file:
+
+    # Create reader object
+    csv_reader = csv.reader(file, delimiter=",")
+
+    # If header exists, SKIP
+    csv_header = next(csv_reader)
+
+    
 
 
 
