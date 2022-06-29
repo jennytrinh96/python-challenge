b8b9c630f0cf54ba2306f158d1c81479f7a47c38
Jenny Bedwell <104868749+jennytrinh96@users.noreply.github.com>
2022-06-29 01:23:38 -0400

    total months
    calculated total months

--- PyBank/main.py
+++ PyBank/main.py
@@ -6,19 +6,38 @@ import csv
 import os
 
 # Set up file path for csv file
-csv_path = os.path.join("..", "Resources", "budget_data.csv")
+budget_csv_path = os.path.join("Resources", "budget_data.csv")
 # print(csv_path) Check path, Path is good to go
 
+
+# Variables to use: 
+total_months = 0 
+# Accumulator for months
+
+
 # FIRST: Open csv file so we can search and do analysis
-with open(csv_path, 'r') as file:
+with open(budget_csv_path) as budget_file:
 
     # Create reader object
-    csv_reader = csv.reader(file, delimiter=",")
+    csv_reader = csv.reader(budget_file)
 
     # If header exists, SKIP
     csv_header = next(csv_reader)
 
-    
+    # Increment the count of total months
+    total_months += 1
+
+    # Create for loop to read each row
+    for row in csv_reader:
+        total_months += 1
+
+
+# Generate the output
+output = f"Total Months = {total_months}"
+print(output)
+
+
+
 
 
 
